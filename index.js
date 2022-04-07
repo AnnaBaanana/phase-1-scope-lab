4a5f68274e0ac4ea109c842aebe5a388d582bdd9
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-07 17:21:12 -0400

    commit tree 6354e31dfbf3b73e1de3bbc301ecc83f68d1724e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName()
+{
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer="not bob"
+}
+
+setBestCustomer();
+console.log(bestCustomer)
+
+function overwriteBestCustomer(name) {
+    bestCustomer=name;
+}
+
+overwriteBestCustomer("maybe bob");
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'matt';
+
+function changeLeastFavoriteCustomer(name) {
+    leastFavoriteCustomer=name;
+}
+
+changeLeastFavoriteCustomer('brad')
